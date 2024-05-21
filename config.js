124507726e18da948fda0a66c2628de424b473fd
Brown boy <139825550+Msksmsnsnjs@users.noreply.github.com>
2024-05-21 23:34:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,13 +16,13 @@ global.autoreadgc = process.env.AUTOREADGC || true;
 
 
 // Auth information
-global.pairNumber = "919002162806";                         // Add your paining number with country code example "916297175943"; 
+global.pairNumber = "919654054457";                         // Add your paining number with country code example "916297175943"; 
 global.port = process.env.PORT || "10000";
 global.auth = process.env.AUTH || "Pairing";                // Auth mode OR/Pairing.
 global.sessionFile = process.env.SESSION_FILE || "A17-SESSION";
 global.mongodb = process.env.MONGODB || "";                 // Mongodb url.
-global.website = "https://www.exenoz.tech"; 
-global.github = "https://github.com/Kai0071";
+global.website = ""; 
+global.github = "";
 
 
 // Default prefix
@@ -30,13 +30,13 @@ global.prefa = process.env.PREFIX ? process.env.PREFIX.split(",") : ["."];
 
 
 // Owner information
-global.Owner = process.env.OWNER ? process.env.OWNER.split(",") : ["916297175943", "919362639362"];
-global.OwnerNumber = process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.split(",") : ["916297175943", "919362639362"];
-global.ownertag = process.env.OWNER_TAG ? process.env.OWNER_TAG.split(",") : ["916297175943"];
-global.OwnerName = process.env.OWNER_NAME || "Kai";
-global.BotName = process.env.BOT_NAME || "A17";
-global.packname = process.env.PACK_NAME || "A17 Bot";
-global.author = "By: Kai";
+global.Owner = process.env.OWNER ? process.env.OWNER.split(",") : ["919654054457"];
+global.OwnerNumber = process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.split(",") : ["919654054457"];
+global.ownertag = process.env.OWNER_TAG ? process.env.OWNER_TAG.split(",") : ["919654054457"];
+global.OwnerName = process.env.OWNER_NAME || "LuCAs_xd";
+global.BotName = process.env.BOT_NAME || "LuCAs_xd";
+global.packname = process.env.PACK_NAME || "LuCAs_xd";
+global.author = "LuCAs_xd";
 global.BotSourceCode = "https://github.com/Kai0071/A17";
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
 
